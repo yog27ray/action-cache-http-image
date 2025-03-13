c8b78314a148bda59112036c2b2287b7701fe585
yog27ray <yog27ray@gmail.com>
2025-03-13 11:19:56 +0530

    Base image

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,6 @@ set -e
 
 VERSION=$(cat package.json | jq -r .version)
 
-docker build -t "yog27ray/action-cache-http-image:$VERSION" -t "yog27ray/action-cache-http-image:latest" ./
+docker buildx build --provenance=true --sbom=true -t "yog27ray/action-cache-http-image:$VERSION" -t "yog27ray/action-cache-http-image:latest" ./
 docker push "yog27ray/action-cache-http-image:$VERSION"
 docker push "yog27ray/action-cache-http-image:latest"
