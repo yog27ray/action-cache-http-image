cf5f5e4fc54c187a98b55c789077001f36a587ac
yog27ray <yog27ray@gmail.com>
2025-03-13 18:43:55 +0530

    Base image

--- cache-http.sh
+++ cache-http.sh
@@ -115,8 +115,6 @@ if [ "$response" = "200" ] || [ "$response" -eq 200 ]; then
     echo "Cache hit, untar success"
 else
     echo "Cache miss"
-    whoami
-    ls -lah
     bash -c "$INPUT_INSTALL_COMMAND"
     tar "${COMPRESS_FLAG}cf" "$tarFile" "$INPUT_DESTINATION_FOLDER"
 
